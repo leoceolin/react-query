1f7c6fc67ba9c9fda9535d1c16ad2fb9f7a6dd45
kobe-leonel <leonel.farias@kobe.io>
2024-02-27 09:42:17 -0300

    add commented params

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import axios from "axios";
-import { useQuery, useMutation, useQueryClient } from "react-query";
+import axios from 'axios'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
 
-import "./App.css";
+import './App.css'
 
 function App() {
-  const queryClient = useQueryClient();
+  const queryClient = useQueryClient()
 
-  const { data, isLoading, error } = useQuery("todos", () =>
-    axios.get("http://localhost:8080/todos").then((response) => response.data)
-  );
+  const { data, isLoading, error } = useQuery(
+    'todos',
+    () => {
+      axios.get('http://localhost:8080/todos').then((response) => response.data)
+    },
+    /* {
+      retry: 1, // number of times the request will be dispateched before throw error
+      refetchOnWindowFocus: true, // will dispatch the request when the user focys on the screen
+      refetchInterval: 60 * 60 * 24, // 24 hours
+      initialData: [{ // data to be shown while request isn't finalized
+      "userId": 1,
+      "id": 7,
+      "title": "Initial data",
+      "completed": false
+      }]
+    },*/
+  )
 
   const mutation = useMutation({
     mutationFn: ({ todoId, completed }) => {
@@ -16,24 +30,24 @@ function App() {
         .patch(`http://localhost:8080/todos/${todoId}`, {
           completed,
         })
-        .then((response) => response.data);
+        .then((response) => response.data)
     },
     onSuccess: (data) => {
-      queryClient.setQueryData("todos", (currentData) =>
-        currentData.map((todo) => (todo.id === data.id ? data : todo))
-      );
+      queryClient.setQueryData('todos', (currentData) =>
+        currentData.map((todo) => (todo.id === data.id ? data : todo)),
+      )
     },
     onError: (error) => {
-      console.error(error);
+      console.error(error)
     },
-  });
+  })
 
   if (isLoading) {
-    return <div className="loading">Loading...</div>;
+    return <div className="loading">Loading...</div>
   }
 
   if (error) {
-    return <div className="loading">Something went wrong!</div>;
+    return <div className="loading">Something went wrong!</div>
   }
 
   return (
@@ -45,7 +59,7 @@ function App() {
             onClick={() =>
               mutation.mutate({ todoId: todo.id, completed: !todo.completed })
             }
-            className={`todo ${todo.completed && "todo-completed"}`}
+            className={`todo ${todo.completed && 'todo-completed'}`}
             key={todo.id}
           >
             {todo.title}
@@ -53,7 +67,7 @@ function App() {
         ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
